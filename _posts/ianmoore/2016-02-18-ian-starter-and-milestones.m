bcba803b30555ea78cac5b5a9b23b9775e2bb77b
touchwick <igamoore@gmail.com>
2016-02-17 20:17:14 -0500

    Create Ian's starter code and milestones post

---
+++ _posts/ianmoore/2016-02-18-ian-starter-and-milestones.m
@@ -0,0 +1,16 @@
+---
+layout: post
+author: touchwick
+title: "Ian's Milestones and Starter Code"
+---
+[x] Build starter code
+[] Working interface - Buttons respond to clicks, mode buttons clear when one mode is selected.
+[] Mode 1 - First mode will draw
+[] Mode 2 - Second mode will draw
+[] Mode 3 - Third mode will draw
+[] Clear and reset - Clear button functions correctly, program resets to initial state, ready for user to reinitiate
+[] Debugging - Check to make sure that everything <i>actually works</i>.
+
+My starter code is below:
+
+<iframe src="https://trinket.io/embed/python/f82adb56c8" width="100%" height="600" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen></iframe>
